502254f3f8b7f0f60f1bfc1c7fb3ec43da0a4754
Diego G <diegogalvis0905@hotmail.com>
2022-10-24 18:25:24 -0500

    File

--- main.py
+++ main.py
@@ -1,6 +1,49 @@
-class Node:
+
+
+class Nodo:
     def __init__(self, data):
         self.left = None
         self.right = None
         self.data = data
-        
+
+    def insert(self, data):
+        if self.data:
+            if data < self.data:
+                if self.left == None:
+                    self.left = Nodo(data)
+                else:
+                    self.left.insert(data)
+            elif data > self.data:
+                if self.right == None:
+                        self.right = Nodo(data)
+                else:
+                        self.right.insert(data)
+        else:
+            self.data = data
+       
+# Print the Tree
+    def PrintTree(self):
+      if self.left:
+         self.left.PrintTree()
+      print( self.data),
+      if self.right:
+         self.right.PrintTree()
+# Inorder traversal
+# Left -> Root -> Right
+    def inorderTraversal(self, root):
+      res = []
+      if root:
+         res = self.inorderTraversal(root.left)
+         res.append(root.data)
+         res = res + self.inorderTraversal(root.right)
+      return res
+
+f = open("archivo.txt", "r")
+encabezador = Nodo("Indice libro")
+
+for x in f:
+    encabezador.insert(x)
+
+print(encabezador.inorderTraversal(encabezador))
+
+
