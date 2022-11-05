f0947e851128372c0ceb9c21e24cf859d003a52c
Diego G <diegogalvis0905@hotmail.com>
2022-11-04 22:20:15 -0500

    2.0

--- Arbol.py
+++ Arbol.py
@@ -6,7 +6,7 @@ class Arbol:
         self.raiz = Nodo(data)
         
     def insertarRecursivo(self, nodo, data):
-        
+
         if self.raiz == None:
             self.raiz = Nodo(data)
         else:
@@ -26,12 +26,58 @@ class Arbol:
     def insertar(self, data):
         self.insertarRecursivo(self.raiz, data)
         
+    def insertarSubTRecursivo(self, nodo, arbol):
+        if nodo == None:
+            return
+        else:
+            if nodo.subT == None:
+                nodo.subT = arbol
+            else:
+                self.insertarSubTRecursivo(nodo.izq, arbol)
+                self.insertarSubTRecursivo(nodo.der, arbol)
+                   
+    def insertarSubT(self, arbol):
+        self.insertarSubTRecursivo(self.raiz, arbol)
+        
+    def insertarPagesRecursivo(self, nodo, arbol):
+        if nodo == None:
+            return
+        else:
+            if nodo.subT == None:
+                nodo.subT = arbol
+            else:
+                self.insertarSubTRecursivo(nodo.izq, arbol)
+                self.insertarSubTRecursivo(nodo.der, arbol)
+    
+    def insertarPages(self, arbol):
+        self.insertarPagesRecursivo(self.raiz, arbol)
+    
+    def imprimir(self):
+        self.imprimirRecursivo(self.raiz)
         
     def imprimirRecursivo(self, nodo):
         if nodo != None:
             self.imprimirRecursivo(nodo.izq)
-            print(nodo.data, end = ", ")
+            self.imprimirSubTRecursivo(nodo.subT)
+            print(nodo.data, end =  ",")
+            print(nodo.subT, end = ",")
             self.imprimirRecursivo(nodo.der)
-    
-    def imprimir(self):
-        self.imprimirRecursivo(self.raiz)
+            self.imprimirRecursivo(nodo.subT)
+        else:
+            return
+        
+    def imprimirSubTRecursivo(self, arbol):
+        if arbol != None:
+            self.imprimirSubTotalRecursivo(arbol.raiz.izq)
+            print(arbol.raiz.data)
+            self.imprimirSubTotalRecursivo(arbol.raiz.der)
+        else:
+            return
+            
+    def imprimirSubTotalRecursivo(self, nodo):
+        if nodo != None:
+            self.imprimirSubTotalRecursivo(nodo.izq)
+            print(nodo.data)
+            self.imprimirSubTotalRecursivo(nodo.der)   
+        else:
+            return    
--- main.py
+++ main.py
@@ -1,39 +1,72 @@
+# Restrictions:
+#     1. First element of txt file gotta be a term
+#     2. Second elemento txt file gotta be a subterm
+#     3. Sub/term can only apear in 9 pages
+#     4. if appear iin a page of one digit always put a zerobefore the number, example: 09,01
+
 from Arbol import Arbol
 import re
 
 f = open("Archivo.txt")
 words = f.readlines()
 
-if (words[0])[0:1] == "m":
-    treeM = Arbol(words[0])
+allesWords = []
 
-for i in range(len(words)):
+for n in words:
+    regexA = re.split('(\d+)', n)
+    allesWords.append((regexA[0])[1:])
+    
+
+if (words[0])[0:1] == "m":
+    treeM = Arbol(allesWords[0])
+    
+counter = 0
+alles = []
+
+print(allesWords)
+
+for line in words: #
+    if line[0:1] == "n" or line[0:1] == "s": #If word stars with n o s put number in alles[]
+        regex = re.split('(\d+)', line)#list: separated words and numbers
+        alles.append(regex[1])
+        
+
+for i in range(len(allesWords)):
     w = words[i]
     
     if w[0:1] == "m":
-        treeM.insertar(words[i])
-        wP = words[i+1] #Second input gotta be always a subterm
-        treeN = Arbol(wP)
-        treeM.raiz.pages = treeN
+        treeM.insertar(allesWords[i]) #Insert term m in main tree 
+        wP = allesWords[i+1] #Next word after M term
+        treeN = Arbol(wP) 
+        treeM.raiz.subT = treeN  
         j = i+2
         wP = words[j]
         
+        treeP = Arbol(alles[counter])
+        treeN.raiz.pages = treeP
+
         while (wP[0:1] == "n" or wP[0:1] == "s"):
+            counter+=1
+            subTRegex = re.split('(\d+)', wP)
+            wP = (subTRegex[0])[1:]
             treeN.insertar(wP)
+            treeP.insertar(alles[counter])
             j+=1
             
             if j < len(words):
                 wP = words[j]
             else:
                 break
-
-                
-# for line in f:
-#     word = f.readline()
-    
-#     if word[0:1] == "m":
-#         wordSplit = re.split(r'(\d+)', word)
-#         root.insertar(wordSplit)
+            
+        #Go through all the tree and check the one that has .subT == None
+        #Insert treeN to that
+        treeM.insertarSubT(treeN)
+        #treeN.insertarPages(treeP)
+        
+# treeM.imprimir()
 
 treeM.imprimir()
-treeM.raiz.der.pages.imprimir()
+#print("\n")
+#treeN.imprimir()
+
+#treeP.raiz.izq.pages.imprimir()
